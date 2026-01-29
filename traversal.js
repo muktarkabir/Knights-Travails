b857a11e12b631c1f55fa3bea91bca558749c2f4
muktarkabir <mukhtarkabir3394@gmail.com>
2026-01-29 13:12:41 +0100

    Create function for finding the possible moves from a position.

--- traversal.js
+++ traversal.js
@@ -18,18 +18,45 @@ function kinghtMoves(start, destination) {
       throw new Error("Point must be between 0 and 7");
     }
   });
+
+  //So now that my coordinates are valid, what next??
+  //First record every position i can visit from the start position
+  //Then explore each one until the destination is reached.
+  //The shortest of these positionsis my answer.
+}
+
+function possibleMovesFromAPosition(position = [0, 6]) {
+  let arrayOfMoves = [];
+  arrayOfMoves.push([position[0] + 2, position[1] + 1]);
+  arrayOfMoves.push([position[0] + 2, position[1] - 1]);
+  arrayOfMoves.push([position[0] - 2, position[1] + 1]);
+  arrayOfMoves.push([position[0] - 2, position[1] - 1]);
+
+  arrayOfMoves.push([position[0] + 1, position[1] + 2]);
+  arrayOfMoves.push([position[0] - 1, position[1] + 2]);
+  arrayOfMoves.push([position[0] + 1, position[1] - 2]);
+  arrayOfMoves.push([position[0] - 1, position[1] - 2]);
+  const filtered = arrayOfMoves.filter(
+    (move) => move[0] >= 0 && move[0] <= 7 && move[1] >= 0 && move[1] <= 7,
+  );
+
+  console.log(arrayOfMoves);
+  console.log("__________________-----_________");
+  console.log(filtered);
 }
 
 const graph = [
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 1, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
+// 0  1  2  3  4  5  6  7
+  [0, 0, 0, 0, 0, 0, 0, 0], //0
+  [0, 0, 0, 0, 0, 0, 0, 0], //1
+  [0, 0, 0, 0, 0, 0, 0, 0], //2
+  [0, 0, 0, 0, 0, 0, 0, 0], //3
+  [0, 0, 0, 0, 0, 0, 0, 0], //4
+  [0, 0, 0, 0, 1, 0, 0, 0], //5
+  [0, 0, 0, 0, 0, 0, 0, 0], //6
+  [0, 0, 0, 0, 0, 0, 0, 0], //7
 ];
+//[7, 4],[7, 2],[3, 4],[3, 2],[6, 5],[4, 5],[6, 1],[4, 1]
 
 //Use a queue
 //Something like queue = [],
@@ -38,4 +65,5 @@ const graph = [
 //Something like visitedPositions = [],
 //  when traversing check if position is  not in visitedPosition to proceed,
 //  otherwise skip and move to next position.
-kinghtMoves([8, 3], [4, 2]);
+kinghtMoves([5, 3], [4, 2]);
+possibleMovesFromAPosition();
