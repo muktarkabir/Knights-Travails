fc021a766464466fae0a0232bd291a356f4289ff
mukhtarkabir <mukhtarkabir3394@gmail.com>
2026-05-06 17:11:45 +0100

    Reduce repition of code.

--- traversal.js
+++ traversal.js
@@ -4,21 +4,13 @@ function kinghtMoves(start, destination) {
     throw new Error("Provide arrays of coordinates for start and destination");
   if (start.length !== 2 || destination.length !== 2)
     throw new Error("Each coordinate should have two numbers between 0 and 7");
-  start.forEach((coord) => {
+  [...start,...destination].forEach((coord) => {
     if (typeof coord !== "number") {
       throw new Error("Coordinate points must be numbers");
     } else if (coord < 0 || coord > 7) {
       throw new Error("Point must be between 0 and 7");
     }
   });
-  destination.forEach((coord) => {
-    if (typeof coord !== "number") {
-      throw new Error("Coordinate points must be numbers");
-    } else if (coord < 0 || coord > 7) {
-      throw new Error("Point must be between 0 and 7");
-    }
-  });
-
   //So now that my coordinates are valid, what next??
   //First record every position i can visit from the start position
   //Then explore each one until the destination is reached.
